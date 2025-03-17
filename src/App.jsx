b697ea96b0917e50ec8f0f57e13267c8bcc26afd
AlmasriMohammad <54584958+AlmasriMohammad@users.noreply.github.com>
2025-03-17 18:54:14 -0400

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import "./index.css";
-import Chat from "./components/chat/Chat";
-import Detail from "./components/detail/Detail";
-import List from "./components/list/List";
+import Chat from "./components/chat/Chat"
+import Detail from "./components/detail/Detail"
+import List from "./components/list/List"
+import Login from "./components/login/Login"
 
 const App = () => {
-  const user = true;
+
+  const user = false
 
   return (
     <div className='container'>
+      {
+        user ?(
+          <>
       <List/>
       <Chat/>
       <Detail/>
+      </>
+        ) : (<Login />)
+      }
     </div>
   )
 }
