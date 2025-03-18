c2f06d9499f38083cd5e3bb263ed2cb99146892e
AlmasriMohammad <54584958+AlmasriMohammad@users.noreply.github.com>
2025-03-18 18:05:43 -0400

    Firebase User Setup, login page completed, unique user page setup

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,46 +1,96 @@
 import { useState } from "react";
 import "./login.css";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+import { auth, db } from "../../lib/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 const Login = () => {
   const [avatar, setAvatar] = useState({
     file: null,
-    url: ""
+    url: "",
   });
+
+  const [loading, setLoading] = useState(false);
 
   const handleAvatar = (e) => {
     if (e.target.files[0]) {
       setAvatar({
         file: e.target.files[0],
-        url: URL.createObjectURL(e.target.files[0])
+        url: URL.createObjectURL(e.target.files[0]),
       });
     }
   };
 
+  const handleRegister = async (e) => {
+    
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+        email,
+        id: res.user.uid,
+        blocked:[],
+      });
+
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats:[],
+      });
+
+      toast.success("Account created! ");
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
+
+    try{
+      await signInWithEmailAndPassword(auth,email,password);
+      toast.success("Account created! ");
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false)
+    }
+  };
+
   return (
-    <div className="login-container">
+    <div className="login">
       <div className="item">
-        <h2>Welcome back</h2>
-        <form>
+        <h2>Welcome back,</h2>
+        <form onSubmit={handleLogin}>
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Sign In</button>
+          <button> Sign in </button>
         </form>
       </div>
-
-      <div className="seperator"></div>
-
+      <div className="separator"></div>
       <div className="item">
-        <h2>Create an account</h2>
-        <form>
-          <label htmlFor="file" className="avatar-label">
-            <img src={avatar.url || "/avatar.png"} alt="Avatar Preview" className="avatar-img" />
+        <h2>Create an Account</h2>
+        <form onSubmit={handleRegister}>
+          <label htmlFor="file">
+            <img src={avatar.url || "./avatar.png"} alt="" />
             Upload an image
           </label>
-          <input type="file" id="file" style={{ display: "none" }} onChange={handleAvatar} />
+          <input type="file" id="file" style={{display:"none"}} onChange={handleAvatar}/>
           <input type="text" placeholder="Username" name="username" />
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Sign Up</button>
+          <button> Sign Up </button>
         </form>
       </div>
     </div>
