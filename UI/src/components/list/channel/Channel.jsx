22f0a0c3167361383aa3c15f430378642b654561
timele55 <120067069+timele55@users.noreply.github.com>
2025-03-21 14:43:14 -0400

    Update Channel.jsx
    Update Channel for Sprint 3.

--- UI/src/components/list/channel/Channel.jsx
+++ UI/src/components/list/channel/Channel.jsx
@@ -3,12 +3,18 @@ import ChannelList from "./channelList/ChannelList";
 import ChannelInfo from "./channelInfo/ChannelInfo";
 import "./channel.css";
 
-const Channel = ({ selectedChannel, setSelectedChannel }) => {
+const Channel = ({ selectedChannel, setSelectedChannel, setSelectedServer }) => {
+
   const [currentServerId, setCurrentServerId] = useState(null);
+
+  const handleServerSelect = (serverId) => {
+    setCurrentServerId(serverId); 
+    setSelectedServer(serverId);  //Update selectedServer in App.jsx
+};
 
   return (
     <div className="channel">
-      <ChannelList setCurrentServerId={setCurrentServerId} currentServerId={currentServerId} />
+      <ChannelList setCurrentServerId={handleServerSelect} currentServerId={currentServerId} />
       {currentServerId ? (
         <ChannelInfo 
           selectedChannel={selectedChannel}
