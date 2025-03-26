7ec455866293f7935f716a7c2b87c99a893bdb22
timele55 <120067069+timele55@users.noreply.github.com>
2025-03-25 23:48:30 -0400

    Update App.jsx
    Modifying App.jsx to accomodate user login.

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,15 +1,45 @@
-import React, { useState } from "react";
-import List from "./components/list/List";
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 import Chat from "./components/chat/Chat";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import { useUserStore } from "./userStore";
+import { useChatStore } from "./chatStore";
 
 const App = () => {
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
+
   const [selectedChannel, setSelectedChannel] = useState(null);
   const [selectedServer, setSelectedServer] = useState(null);
 
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
   return (
-    <div className='container'>
-      <List selectedChannel={selectedChannel} setSelectedChannel={setSelectedChannel} setSelectedServer={setSelectedServer} /> {/* Pass function to List */}
-      <Chat channelId={selectedChannel} serverId={selectedServer}/> {/* Pass selectedChannel to Chat */}
+    <div className="container">
+      {currentUser ? (
+        <>
+          <List 
+            selectedChannel={selectedChannel} 
+            setSelectedChannel={setSelectedChannel} 
+            setSelectedServer={setSelectedServer} 
+          />
+          {<Chat channelId={selectedChannel} serverId={selectedServer} />}
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 };
